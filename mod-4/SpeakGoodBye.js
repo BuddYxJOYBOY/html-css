9c36d2775487e692636b5d2d33aadf5151fab907
BuddYxJOYBOY <130282406+BuddYxJOYBOY@users.noreply.github.com>
2023-06-06 16:35:53 +0530

    Update SpeakGoodBye.js

--- mod-4/SpeakGoodBye.js
+++ mod-4/SpeakGoodBye.js
@@ -9,8 +9,9 @@
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 // var byeSpeaker =
-
+var byeSpeaker = speak(name);
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+(function (){
 var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
@@ -19,7 +20,7 @@ var speakWord = "Good Bye";
 function speak(name) {
   console.log(speakWord + " " + name);
 }
-
+})();
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
